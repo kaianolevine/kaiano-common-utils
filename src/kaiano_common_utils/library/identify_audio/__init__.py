eb46809b4ac095aff00eacc65d0def151a19a259
Kaiano Levine <kaiano.levine@gmail.com>
2026-01-15 17:18:26 -0600

    Updating init

--- src/kaiano_common_utils/library/identify_audio/__init__.py
+++ src/kaiano_common_utils/library/identify_audio/__init__.py
@@ -1,14 +1,20 @@
 """Unified audio identification + tagging + renaming interface."""
 
 from .api import IdentifyAudio
-from .policies import IdentificationPolicy, RenamePolicy, TagPolicy
-from .result import ProcessResult, ProcessWarning
+from .policies import IdentificationPolicy
+
+# Low-level interfaces (advanced / escape-hatch use only)
+from .retagger_api import AcoustIdIdentifier, MusicBrainzRecordingProvider
+from .retagger_music_tag import MusicTagIO
+from .retagger_types import TagSnapshot, TrackId, TrackMetadata
 
 __all__ = [
     "IdentifyAudio",
-    "TagPolicy",
-    "RenamePolicy",
     "IdentificationPolicy",
-    "ProcessResult",
-    "ProcessWarning",
+    "TagSnapshot",
+    "TrackId",
+    "TrackMetadata",
+    "AcoustIdIdentifier",
+    "MusicBrainzRecordingProvider",
+    "MusicTagIO",
 ]
